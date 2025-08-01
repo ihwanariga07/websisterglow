d350cba704d5e04d684172f902efe9a524d03d46
Ihwan Ariga <ihwanariga.ia@gmail.com>
2025-08-02 01:20:08 +0700

    memasukkan are untuk setelah login di route

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,24 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-// Route::redirect('/', '/login');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/login');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/* ---------- Area setelah login ---------- */
+Route::middleware('auth')->group(function () {
+
+    Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+});
+
+
+
+
+
+
+});
